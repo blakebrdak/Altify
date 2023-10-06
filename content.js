760fa953fd9bc44964c635d1027c7b87b6e80009
blakebrdak <bbrdak@umich.edu>
2023-10-06 11:16:52 -0400

    slight changes

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
-console.log('extension doing shit bro');
 let images = document.getElementsByTagName('img');
 for (img of images) {
-    if (!img.alt || img.alt === "") {
-        img.alt = "This image was missing alt text... bummer."
+    if (!img.alt || img.alt === ""){
+        // GOOGLE CLOUD VISION PROMPT
+        let labels = ['cat', 'dog']
+        // Open AI API call
+        // OPENAI result is 
+        img.alt = "This image was missing alt text... bummer." // Save openAI result here
     }
 }

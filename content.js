ab6b786f3fb810c249b1dbcce74929007eb6c016
blakebrdak <bbrdak@umich.edu>
2023-10-10 15:12:26 -0400

    fixed key

--- content.js
+++ content.js
@@ -48,7 +48,7 @@ async function extractKeyContentAndTrim(img, maxWordCount) {
 // Function to make an asynchronous API call to the OpenAI API
 // may need to avoid hard coding the openAI key in this script
 async function openAI_API_Completions(prompt) {
-    const API_KEY = "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";  // Replace with OpenAI API key
+    const API_KEY = "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";  // Replace with OpenAI API key
     // Ensure the API key is provided
     if (!API_KEY) {
         console.error("Please provide your OpenAI API key.");
